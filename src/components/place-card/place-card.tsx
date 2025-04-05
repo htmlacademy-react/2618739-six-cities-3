6f7daba8976ad86ef9fb7be341328833fd9a04a1
Petr Karasev <petr.karasev@datadvance.net>
2025-04-06 02:03:56 +0300

    props for favorites

--- src/components/place-card/place-card.tsx
+++ src/components/place-card/place-card.tsx
@@ -1,9 +1,11 @@
 import PlaceCardInfo from './place-card__info';
 import TOffer from '../../types/offers';
+import { Link } from "react-router-dom";
 
 type PlaceCardProps = { offersProp: TOffer }
 
 function PlaceCard({ offersProp }: PlaceCardProps): JSX.Element {
+  offer_path = "/offer/" + offersProp.id
   return (
     <article className="cities__card place-card">
       <div className="place-card__mark">
@@ -11,11 +13,11 @@ function PlaceCard({ offersProp }: PlaceCardProps): JSX.Element {
           <span>Premium</span> : null}
       </div>
       <div className="cities__image-wrapper place-card__image-wrapper">
-        <a href="#">
-          <img className="place-card__image" src="img/apartment-01.jpg" width="260" height="200" alt="Place image" />
-        </a>
+        <Link to={offer_path} >
+          <img className="place-card__image" src={offersProp.images[0]} width="260" height="200" alt="Place image" />
+        </Link>
       </div>
       <PlaceCardInfo offersProp={offersProp} />
-    </article>);
+    </article >);
 }
 export default PlaceCard;
