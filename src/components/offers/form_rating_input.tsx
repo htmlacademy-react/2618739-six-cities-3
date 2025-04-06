6fc9f0b24218f8c598d72512ed86cdcd4fc50b0e
Petr Karasev <petr.karasev@datadvance.net>
2025-04-06 20:09:37 +0300

    Fix for id in rating input

--- src/components/offers/form_rating_input.tsx
+++ src/components/offers/form_rating_input.tsx
@@ -1,7 +1,8 @@
 type RatingValue = { value: number };
 function FormRatingInput({ value }: RatingValue): JSX.Element {
+  const id = `${value.toString() }-stars`;
   return (
-    <input className="form__rating-input visually-hidden" name="rating" value={value} id="{value}-stars" type="radio"/>
+    <input className="form__rating-input visually-hidden" name="rating" value={value} id={id} type="radio" />
   );
 }
 export default FormRatingInput;
