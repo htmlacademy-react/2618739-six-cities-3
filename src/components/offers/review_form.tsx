56fc2fdceca4d1166dc3b40b5aeefb652370badb
Petr Karasev <petr.karasev@datadvance.net>
2025-04-06 20:06:14 +0300

    Fix for stars

--- src/components/offers/review_form.tsx
+++ src/components/offers/review_form.tsx
@@ -14,16 +14,16 @@ function ReviewsForm(): JSX.Element {
     <form className="reviews__form form" action="#" method="post">
       <label className="reviews__label form__label" htmlFor="review">Your review</label>
       <div className="reviews__rating-form form__rating">
-        <FormRatingInput value={5} setRating={setStars} />
-        <Star for_star='5-stars' title_star='perfect' setRating={() => setStars(5)} />
-        <FormRatingInput value={4} setRating={setStars} />
-        <Star for_star='4-stars' title_star='good' setRating={() => setStars(4)} />
-        <FormRatingInput value={3} setRating={setStars} />
-        <Star for_star='3-stars' title_star='not bad' setRating={() => setStars(3)} />
-        <FormRatingInput value={2} setRating={setStars} />
-        <Star for_star='2-stars' title_star='badly' setRating={() => setStars(2)} />
-        <FormRatingInput value={1} setRating={setStars} />
-        <Star for_star='1-stars' title_star='terribly' setRating={() => setStars(1)} />
+        <FormRatingInput value={5} />
+        <Star for_star={'5-stars'} title_star={'perfect'} setRating={() => setStars(5)} />
+        <FormRatingInput value={4} />
+        <Star for_star={'4-stars'} title_star={'good'} setRating={() => setStars(4)} />
+        <FormRatingInput value={3} />
+        <Star for_star={'3-stars'} title_star={'not bad'} setRating={() => setStars(3)} />
+        <FormRatingInput value={2} />
+        <Star for_star={'2-stars'} title_star={'badly'} setRating={() => setStars(2)} />
+        <FormRatingInput value={1} />
+        <Star for_star={'1-stars'} title_star={'terribly'} setRating={() => setStars(1)} />
       </div>
       <textarea className="reviews__textarea form__textarea" id="review" name="review" placeholder="Tell how was your stay, what you like and what can be improved"></textarea>
       <div className="reviews__button-wrapper">
