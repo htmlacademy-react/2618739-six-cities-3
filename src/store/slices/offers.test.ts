21dbedee6b4ea67f9f070511e3b0298637905dcd
Petr Karasev <petr.karasev@datadvance.net>
2025-07-02 15:37:29 +0300

    First test

---
+++ src/store/slices/offers.test.ts
@@ -0,0 +1,21 @@
+import { OffersSlice } from './offers';
+import { RequestStatus, CITIES } from '../../const';
+import { Sorting } from '../../types/sorting';
+
+describe("Offers slice tests", () => {
+    it('should return initial state with empty action', () => {
+        const emptyAction = { type: '' };
+        const expectedState = {
+            city: CITIES[0].title,
+            offers: [],
+            activeOffer: 0,
+            status: RequestStatus.Idle,
+            sorting: Sorting.Default,
+            favorites: []
+        };
+
+        const result = OffersSlice.reducer(expectedState, emptyAction);
+        console.log(result);
+        expect(result).toEqual(expectedState);
+    });
+});
