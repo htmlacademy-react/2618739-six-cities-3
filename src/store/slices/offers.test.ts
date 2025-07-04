095f502d150ffd9f536e73a3ef3c5daeaa06cf99
Petr Karasev <petr.karasev@datadvance.net>
2025-07-04 16:44:14 +0300

    More tests

--- src/store/slices/offers.test.ts
+++ src/store/slices/offers.test.ts
@@ -17,4 +17,27 @@ describe("Offers slice tests", () => {
         const result = OffersSlice.reducer(expectedState, emptyAction);
         expect(result).toEqual(expectedState);
     });
+    it('should return initial state with empty state and empty action', () => {
+        const emptyAction = { type: '' };
+        const expectedState = {
+            city: CITIES[0].title,
+            offers: [],
+            activeOffer: 0,
+            status: RequestStatus.Idle,
+            sorting: Sorting.Default,
+            favorites: []
+        };
+
+        const result = OffersSlice.reducer(undefined, emptyAction);
+        expect(result).toEqual(expectedState);
+    });
+    it("choosing city", () => {
+        const result = OffersSlice.reducer(undefined, OffersSlice.actions.setCity(CITIES[1].title));
+        expect(result.city).toEqual(CITIES[1].title);
+    })
+    it("set sorting", () => {
+        const result = OffersSlice.reducer(undefined, OffersSlice.actions.setSorting(Sorting.Hight));
+        expect(result.sorting).toEqual(Sorting.Hight);
+    })
+
 });
