822be7e1d93d1df3a41d73a68d4b4711767b7eda
Petr Karasev <petr.karasev@datadvance.net>
2025-07-10 21:34:29 +0300

    More tests

--- src/store/slices/offers.test.ts
+++ src/store/slices/offers.test.ts
@@ -1,6 +1,8 @@
 import { OffersSlice } from './offers';
 import { RequestStatus, CITIES } from '../../const';
 import { Sorting } from '../../types/sorting';
+import { fetchOfferAction } from '../api-actions';
+import { fetchMockOffers } from '../../mock/offers';
 
 describe("Offers slice tests", () => {
     it('should return initial state with empty action', () => {
@@ -42,5 +44,86 @@ describe("Offers slice tests", () => {
     it("set active offer", () => {
         const result = OffersSlice.reducer(undefined, OffersSlice.actions.setActiveOffer(1));
         expect(result.activeOffer).toEqual(1);
+    });
+    it("should set status Loading on fetchOfferAction pending", () => {
+        const initialState = {
+            city: CITIES[0].title,
+            offers: [],
+            activeOffer: 0,
+            status: RequestStatus.Idle,
+            sorting: Sorting.Default,
+            favorites: []
+        };
+        const expectedState = {
+            city: CITIES[0].title,
+            offers: [],
+            activeOffer: 0,
+            status: RequestStatus.Loading,
+            sorting: Sorting.Default,
+            favorites: []
+        };
+        const result = OffersSlice.reducer(initialState, fetchOfferAction.pending);
+        expect(result).toEqual(expectedState);
+    });
+    it("should set status Loading on fetchOfferAction pending", () => {
+        const initialState = {
+            city: CITIES[0].title,
+            offers: [],
+            activeOffer: 0,
+            status: RequestStatus.Idle,
+            sorting: Sorting.Default,
+            favorites: []
+        };
+        const expectedState = {
+            city: CITIES[0].title,
+            offers: [],
+            activeOffer: 0,
+            status: RequestStatus.Loading,
+            sorting: Sorting.Default,
+            favorites: []
+        };
+        const result = OffersSlice.reducer(initialState, fetchOfferAction.pending);
+        expect(result).toEqual(expectedState);
+    });
+    it("should set status Sucess on fetchOfferAction fulfilled", () => {
+        const initialState = {
+            city: CITIES[0].title,
+            offers: [],
+            activeOffer: 0,
+            status: RequestStatus.Idle,
+            sorting: Sorting.Default,
+            favorites: []
+        };
+        const mockOffers = fetchMockOffers();
+        const expectedState = {
+            city: CITIES[0].title,
+            offers: mockOffers,
+            activeOffer: 0,
+            status: RequestStatus.Success,
+            sorting: Sorting.Default,
+            favorites: []
+        };
+        const result = OffersSlice.reducer(initialState, fetchOfferAction.fulfilled(mockOffers, '', undefined));
+        expect(result).toEqual(expectedState);
+    });
+    it("should set status Failed on fetchOfferAction rejected", () => {
+        const initialState = {
+            city: CITIES[0].title,
+            offers: [],
+            activeOffer: 0,
+            status: RequestStatus.Idle,
+            sorting: Sorting.Default,
+            favorites: []
+        };
+        const expectedState = {
+            city: CITIES[0].title,
+            offers: [],
+            activeOffer: 0,
+            status: RequestStatus.Failed,
+            sorting: Sorting.Default,
+            favorites: []
+        };
+        const result = OffersSlice.reducer(initialState, fetchOfferAction.rejected);
+        expect(result).toEqual(expectedState);
     });
 });
