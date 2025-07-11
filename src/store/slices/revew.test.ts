e5ab210e373306539e7a53e7dd5ea900b273ae4d
Petr Karasev <petr.karasev@datadvance.net>
2025-07-11 10:30:54 +0300

    More tests

--- src/store/slices/revew.test.ts
+++ src/store/slices/revew.test.ts
@@ -1,6 +1,6 @@
 import { RequestStatus } from "../../const";
 import { ReviewsSlice } from "./review";
-import { fetchReviewsAction } from "../api-actions";
+import { fetchReviewsAction, putReviewsAction } from "../api-actions";
 import { mockReview } from "../../mock/reviews";
 
 describe("Revew slice tests", () => {
@@ -59,6 +59,43 @@ describe("Revew slice tests", () => {
             status: RequestStatus.Failed
         };
         const result = ReviewsSlice.reducer(initialState, fetchReviewsAction.rejected);
+        expect(result).toEqual(expectedState);
+    });
+    it("should set status Loading on putReviewsAction pending", () => {
+        const initialState = {
+            reviews: [],
+            status: RequestStatus.Idle
+        };
+        const expectedState = {
+            reviews: [],
+            status: RequestStatus.Loading
+        };
+        const result = ReviewsSlice.reducer(initialState, putReviewsAction.pending);
+        expect(result).toEqual(expectedState);
+    });
+    it("should set status Sucess on putReviewsAction fulfilled", () => {
+        const initialState = {
+            reviews: [],
+            status: RequestStatus.Idle
+        };
+
+        const expectedState = {
+            reviews: [mockReview],
+            status: RequestStatus.Success
+        };
+        const result = ReviewsSlice.reducer(initialState, putReviewsAction.fulfilled(mockReview, '', { id: "1", comment: "", rating: 5 }));
+        expect(result).toEqual(expectedState);
+    });
+    it("should set status Failed on putReviewsAction rejected", () => {
+        const initialState = {
+            reviews: [],
+            status: RequestStatus.Idle
+        };
+        const expectedState = {
+            reviews: [],
+            status: RequestStatus.Failed
+        };
+        const result = ReviewsSlice.reducer(initialState, putReviewsAction.rejected);
         expect(result).toEqual(expectedState);
     });
 });
