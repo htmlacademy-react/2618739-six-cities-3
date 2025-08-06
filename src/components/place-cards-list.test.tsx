d3917b777a3c9ebfcef8b1c7a84694b0fc123669
Petr Karasev <petr.karasev@datadvance.net>
2025-08-06 12:49:39 +0300

    More tests

---
+++ src/components/place-cards-list.test.tsx
@@ -0,0 +1,14 @@
+import { render, screen } from '@testing-library/react';
+import PlaceCardList from './place-cards-list';
+import { fetchMockOffer } from '../mock/offers';
+
+
+describe('Component: Header', () => {
+  it('should render correctly', () => {
+    vi.mock('./place-card/place-card');
+    const mockOffers = [fetchMockOffer()]
+    render(<PlaceCardList offersProps={mockOffers} />);
+    expect(screen.getByTestId('card list')).toBeInTheDocument();
+  });
+}
+);
