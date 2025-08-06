8b054157e2c0a29cddb964c8b60cd8498f5494cd
Petr Karasev <petr.karasev@datadvance.net>
2025-08-07 01:27:30 +0300

    Formatting

--- src/components/404.test.tsx
+++ src/components/404.test.tsx
@@ -4,11 +4,12 @@ import { Page404 } from './404';
 
 describe('Component: Page404', () => {
   it('should render correctly', () => {
-    const preparedComponent = (<MemoryRouter>
-      <Routes>
-        <Route path="/" element={<Page404 />} />
-      </Routes>
-    </MemoryRouter>);
+    const preparedComponent = (
+      <MemoryRouter>
+        <Routes>
+          <Route path="/" element={<Page404 />} />
+        </Routes>
+      </MemoryRouter>);
     render(preparedComponent);
     expect(screen.getByText('Ошибка 404. Страница не существует.')).toBeInTheDocument();
   });
