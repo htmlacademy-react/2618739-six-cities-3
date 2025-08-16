5f4a05ffc257bd80055ceafd11354351803a44b6
Petr Karasev <petr.karasev@datadvance.net>
2025-08-17 02:52:43 +0300

    Deleting offers from favorites

--- src/pages/favorites/favorites.tsx
+++ src/pages/favorites/favorites.tsx
@@ -3,10 +3,15 @@ import { useAppSelector } from '../../hooks';
 import { selectBookmarks } from '../../store/selectors/offers';
 import TOffer from '../../types/offers';
 import { Link } from 'react-router-dom';
+import PlaceCardInfo from '../../components/place-card/place-card__info';
 
 function Favorites(): JSX.Element {
   const offers: TOffer[] = useAppSelector(selectBookmarks);
-  const cards = offers.map((offer: TOffer) =>
+  if (offers.length === 0) {
+    return <div>Nothing yet saved</div>
+  }
+  else {
+    const cards = offers.map((offer: TOffer) =>
     (
       <article key={offer.id} className="favorites__card place-card" data-testid='favoritesCard'>
         <div className="place-card__mark">
@@ -19,57 +24,35 @@ function Favorites(): JSX.Element {
             <img className="place-card__image" src={offer.previewImage} width="150" height="110" alt="Place image" />
           </Link>
         </div>
-        <div className="favorites__card-info place-card__info">
-          <div className="place-card__price-wrapper">
-            <div className="place-card__price">
-              <b className="place-card__price-value">&euro;{offer.price}</b>
-              <span className="place-card__price-text">&#47;&nbsp;night</span>
-            </div>
-            <button className="place-card__bookmark-button place-card__bookmark-button--active button" type="button">
-              <svg className="place-card__bookmark-icon" width="18" height="19">
-                <use xlinkHref="#icon-bookmark"></use>
-              </svg>
-              <span className="visually-hidden">In bookmarks</span>
-            </button>
-          </div>
-          <div className="place-card__rating rating">
-            <div className="place-card__stars rating__stars">
-              <span style={{ width: '100%' }}></span>
-              <span className="visually-hidden">Rating</span>
-            </div>
-          </div>
-          <h2 className="place-card__name">
-            <Link to={`/offer/${offer.id}`}>{offer.title}</Link>
-          </h2>
-          <p className="place-card__type">{offer.type}</p>
-        </div>
+        <PlaceCardInfo offersProp={offer} />
       </article >));
-  return (
-    <Private>
-      <main className="page__main page__main--favorites">
-        <div className="page__favorites-container container">
-          <section className="favorites">
-            <h1 className="favorites__title">Saved listing</h1>
-            <ul className="favorites__list">
-              <li className="favorites__locations-items">
-                <div className="favorites__locations locations locations--current">
-                  <div className="locations__item">
-                    <a className="locations__item-link" href="#">
-                      <span>Amsterdam</span>
-                    </a>
+    return (
+      <Private>
+        <main className="page__main page__main--favorites">
+          <div className="page__favorites-container container">
+            <section className="favorites">
+              <h1 className="favorites__title">Saved listing</h1>
+              <ul className="favorites__list">
+                <li className="favorites__locations-items">
+                  <div className="favorites__locations locations locations--current">
+                    <div className="locations__item">
+                      <a className="locations__item-link" href="#">
+                        <span>Amsterdam</span>
+                      </a>
+                    </div>
                   </div>
-                </div>
-                <div className="favorites__places">
-                  {cards}
-                </div>
+                  <div className="favorites__places">
+                    {cards}
+                  </div>
 
-              </li>
-            </ul>
-          </section>
-        </div>
-      </main>
-    </Private >
-  );
+                </li>
+              </ul>
+            </section>
+          </div>
+        </main>
+      </Private >
+    );
+  }
 }
 
 export default Favorites;
