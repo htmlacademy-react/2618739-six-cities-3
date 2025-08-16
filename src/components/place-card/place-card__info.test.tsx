38fc611a41d0604f901f21336e409a37742cbb0a
Petr Karasev <petr.karasev@datadvance.net>
2025-08-16 12:44:51 +0300

    Fix

--- src/components/place-card/place-card__info.test.tsx
+++ src/components/place-card/place-card__info.test.tsx
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import PlaceCardInfo from './place-card__info';
 import { fetchMockOffer } from '../../mock/offers';
-import { withStore } from '../../mock/mock-component';
+import { mockStore, withStore } from '../../mock/mock-component';
 import { AuthorizationStatus, CITIES, RequestStatus } from '../../const';
 import { Sorting } from '../../types/sorting';
 import userEvent from '@testing-library/user-event';
@@ -9,37 +9,14 @@ import * as actions from '../../store/api-actions';
 
 describe('Component: PlaceCardInfo', () => {
   it('should render correctly', () => {
-    const mockOffer = fetchMockOffer();
-    const { withStoreComponent } = withStore(<PlaceCardInfo offersProp={mockOffer} />, {
-      offers:
-      {
-        city: CITIES[0].title,
-        offers: [],
-        favorites: [],
-        activeOfferId: 1,
-        status: RequestStatus.Idle,
-        sorting: Sorting.Default
-      },
-      user: { auth: AuthorizationStatus.Auth }
-    });
+    const { withStoreComponent } = withStore(<PlaceCardInfo offersProp={mockStore.offers.activeOffer} />, mockStore)
     render(withStoreComponent);
-    expect(screen.getByText(mockOffer.title)).toBeInTheDocument();
+    expect(screen.getByText(mockStore.offers.activeOffer.title)).toBeInTheDocument();
   });
   it('should put offer to bookmarks by click on a button', async () => {
     vi.spyOn(actions, 'toBookmarksAction');
     const mockOffer = fetchMockOffer();
-    const { withStoreComponent } = withStore(<PlaceCardInfo offersProp={mockOffer} />, {
-      offers:
-      {
-        city: CITIES[0].title,
-        offers: [],
-        favorites: [],
-        activeOfferId: 1,
-        status: RequestStatus.Idle,
-        sorting: Sorting.Default
-      },
-      user: { auth: AuthorizationStatus.Auth }
-    });
+    const { withStoreComponent } = withStore(<PlaceCardInfo offersProp={mockOffer} />, mockStore);
     render(withStoreComponent);
     await userEvent.click(screen.getByTestId('toBookMarks'));
     expect(actions.toBookmarksAction).toBeCalled();
