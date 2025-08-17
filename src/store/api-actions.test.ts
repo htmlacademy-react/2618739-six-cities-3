644e4f5424b9026c7446ab4a5dd545f5f974731b
Petr Karasev <petr.karasev@datadvance.net>
2025-08-17 14:31:01 +0300

    Test for new API action

--- src/store/api-actions.test.ts
+++ src/store/api-actions.test.ts
@@ -5,7 +5,7 @@ import thunk from 'redux-thunk';
 import { Action } from 'redux';
 import { State, AppThunkDispatch } from '../types/store';
 import { APIRoute } from '../const';
-import { checkAuthAction, fetchOfferAction, fetchReviewsAction, putReviewsAction } from './api-actions';
+import { checkAuthAction, fetchOfferAction, fetchReviewsAction, putReviewsAction, fetchNearOffersAction } from './api-actions';
 import { fetchMockOffer } from '../mock/offers';
 import { mockReview } from '../mock/reviews';
 
@@ -49,6 +49,8 @@ describe('Async actions', () => {
         checkAuthAction.rejected.type,
       ]);
     });
+  });
+  describe('fetchOfferAction', () => {
     it('should dispatch "fetchOfferAction.pending" and "fetchOfferAction.fulfilled" with thunk "fetchOfferAction', async () => {
       const mockOffers = [fetchMockOffer()];
       mockAxiosAdapter.onGet(APIRoute.Offers).reply(200, mockOffers);
@@ -74,6 +76,8 @@ describe('Async actions', () => {
         fetchOfferAction.rejected.type,
       ]);
     });
+  });
+  describe('fetchReviewsAction', () => {
     it('should dispatch "fetchReviewsAction.pending" and "fetchReviewsAction.fulfilled" with thunk "fetchReviewsAction', async () => {
       const id = '1111';
       mockAxiosAdapter.onGet(`${APIRoute.Reviews}/${id}`).reply(200, [mockReview]);
@@ -100,6 +104,8 @@ describe('Async actions', () => {
         fetchReviewsAction.rejected.type,
       ]);
     });
+  });
+  describe('putReviewsAction', () => {
     it('should dispatch "putReviewsAction.pending" and "putReviewsAction.fulfilled" with thunk "putReviewsAction', async () => {
       const id = String(mockReview.id);
       mockAxiosAdapter.onPost(`${APIRoute.Reviews}/${id}`).reply(200, [mockReview]);
@@ -117,6 +123,34 @@ describe('Async actions', () => {
     it('should dispatch "putReviewsAction.pending" and "putReviewsAction.rejected" with thunk "putReviewsAction', async () => {
       const id = '1111';
       mockAxiosAdapter.onPost(`${APIRoute.Reviews}/${id}`).reply(400);
+
+      await store.dispatch(fetchReviewsAction(id));
+      const actions = extractActionsTypes(store.getActions());
+
+      expect(actions).toEqual([
+        fetchReviewsAction.pending.type,
+        fetchReviewsAction.rejected.type,
+      ]);
+    });
+  });
+  describe('fetchNearOffersAction', () => {
+    it('should dispatch "fetchNearOffersAction.pending" and "fetchNearOffersAction.fulfilled" with thunk "putReviewsAction', async () => {
+      const id = String(mockReview.id);
+      mockAxiosAdapter.onGet(`${APIRoute.Offers}/${id}/nearby`).reply(200, [mockReview]);
+
+      await store.dispatch(fetchNearOffersAction(String(mockReview.id)));
+      const actions = store.getActions();
+      const actionTypes = extractActionsTypes(actions);
+      const LoadedReviews = actions.at(1) as ReturnType<typeof putReviewsAction.fulfilled>;
+      expect(actionTypes).toEqual([
+        fetchNearOffersAction.pending.type,
+        fetchNearOffersAction.fulfilled.type,
+      ]);
+      expect(LoadedReviews.payload).toEqual([mockReview]);
+    });
+    it('should dispatch "fetchNearOffersAction.pending" and "fetchNearOffersAction.rejected" with thunk "putReviewsAction', async () => {
+      const id = '1111';
+      mockAxiosAdapter.onGet(`${APIRoute.Offers}/${id}/nearby`).reply(400);
 
       await store.dispatch(fetchReviewsAction(id));
       const actions = extractActionsTypes(store.getActions());
