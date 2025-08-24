042cf3a9d4e63baf23926865409c93e7bbd1494d
Petr Karasev <petr.karasev@datadvance.net>
2025-08-24 03:47:21 +0300

    Validation of login and password

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,10 +1,13 @@
-import { useState, ChangeEvent } from 'react';
+import { useState, ChangeEvent, FormEventHandler } from 'react';
 import { useAppDispatch } from '../../hooks';
 import { login } from '../../store/api-actions';
 import { Link } from 'react-router-dom';
 import { store } from '../../store';
 import { setCity } from '../../store/actions';
 import { CITIES } from '../../const';
+import { FormEvent } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import { validate as validateEmail } from 'email-validator';
 
 
 function Login(): JSX.Element {
@@ -15,10 +18,19 @@ function Login(): JSX.Element {
     setLogin(newLogin);
   };
   const getPassword = (newPassword: string) => {
-    setPassword(newPassword);
+    if (newPassword.length >= 2) {
+      setPassword(newPassword);
+    }
+    else {
+      toast.error("Incorrect password")
+      setPassword("");
+    }
   };
-  const handleSubmit = () => {
-    dispatch(login({ email: userLogin, password: userPassword }));
+  const handleSubmit: FormEventHandler<HTMLFormElement> = (evt: FormEvent<HTMLFormElement>) => {
+    evt.preventDefault();
+    if (userLogin.length > 0 && userPassword.length > 0) {
+      dispatch(login({ email: userLogin, password: userPassword }));
+    }
   };
   const handleEnterPress = (event: React.KeyboardEvent<HTMLElement>) => {
     if (event.key === 'Enter') {
@@ -44,24 +56,32 @@ function Login(): JSX.Element {
         <div className="page__login-container container">
           <section className="login">
             <h1 className="login__title">Sign in</h1>
-            <div className="login__form form">
+            <form className="login__form form" onSubmit={handleSubmit}>
               <div className="login__input-wrapper form__input-wrapper">
                 <label className="visually-hidden">E-mail</label>
-                <input className="login__input form__input" type="email" name="email" data-testid="email" placeholder="Email" required onChange={({ target }: ChangeEvent<HTMLInputElement>) => {
-                  getLogin(target.value);
+                <input className="login__input form__input" type="email" name="email" data-testid="email" placeholder="Email" defaultValue="" required onBlur={({ target }: ChangeEvent<HTMLInputElement>) => {
+                  if (validateEmail(target.value)) {
+                    getLogin(target.value);
+                  }
+                  else {
+                    toast.error("Incorrect e-mail"
+                    );
+                    getLogin('');
+                  }
                 }}
                 />
+                <ToastContainer />
               </div>
               <div className="login__input-wrapper form__input-wrapper">
                 <label className="visually-hidden" data-testid="password">Password</label>
-                <input className="login__input form__input" type="password" name="password" placeholder="Password" required onChange={({ target }: ChangeEvent<HTMLInputElement>) => {
+                <input className="login__input form__input" type="password" name="password" placeholder="Password" required onBlur={({ target }: ChangeEvent<HTMLInputElement>) => {
                   getPassword(target.value);
                 }}
                   onKeyDown={handleEnterPress}
                 />
               </div>
-              <button className="login__submit form__submit button" data-testid="submitButton" onClick={handleSubmit}>Sign in</button>
-            </div>
+              <button className="login__submit form__submit button" data-testid="submitButton" >Sign in</button>
+            </form>
           </section>
           <section className="locations locations--login locations--current">
             <div className="locations__item">
