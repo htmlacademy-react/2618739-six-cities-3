d317c447b9143aefb629e73283d5e186f2ae7ebf
Petr Karasev <petr.karasev@datadvance.net>
2025-08-27 03:03:05 +0300

    Change displaying of map

--- src/const.ts
+++ src/const.ts
@@ -4,58 +4,44 @@ export const URL_MARKER_DEFAULT =
 export const URL_MARKER_CURRENT =
   '/img/pin-active.svg';
 
-export const AMSTERDAM = {
-  title: 'Amsterdam',
-  lat: 52.373057,
-  lng: 4.892557,
-  zoom: 0.5
-};
-
-export const PARIS = {
-  title: 'Paris',
-  lat: 52.373057,
-  lng: 4.892557,
-  zoom: 0.5
-};
-
 
 export const CITIES = [
   {
     title: 'Paris',
     lat: 48.856614,
     lng: 2.3522219,
-    zoom: 0.5
+    zoom: 12
   },
   {
     title: 'Amsterdam',
     lat: 52.373057,
     lng: 4.892557,
-    zoom: 0.5
+    zoom: 12
   },
 
   {
     title: 'Cologne',
     lat: 50.9333,
     lng: 6.95,
-    zoom: 0.5
+    zoom: 12
   },
   {
     title: 'Brussels',
     lat: 50.8504,
     lng: 4.34878,
-    zoom: 0.5
+    zoom: 12
   },
   {
     title: 'Hamburg',
     lat: 53.5753,
     lng: 10.0153,
-    zoom: 0.5
+    zoom: 12
   },
   {
     title: 'Dusseldorf',
     lat: 51.2217,
     lng: 6.77616,
-    zoom: 0.5
+    zoom: 12
   },
 ];
 
